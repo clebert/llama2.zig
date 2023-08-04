2fa9abe9ba4be3141a682a77a3e18ca224edd3b5
Clemens Akens <clebert@me.com>
2023-08-04 17:22:11 +0200

    Add support for CLI args

--- src/main.zig
+++ src/main.zig
@@ -12,20 +12,43 @@ pub fn main() !void {
     defer arena.deinit();
 
     const allocator = arena.allocator();
+
+    // zig build run -Doptimize=ReleaseFast -- <checkpoint_file_path> <steps>=seq_len <prompt>=""
+    var args = try std.process.argsWithAllocator(allocator);
+    var checkpoint_file_path: ?[]const u8 = null;
+    var steps: usize = 0;
+    var prompt: []const u8 = "";
+    var arg_index: usize = 0;
+
+    while (args.next()) |arg| {
+        if (arg_index == 1) {
+            checkpoint_file_path = arg;
+        } else if (arg_index == 2) {
+            steps = try std.fmt.parseInt(usize, arg, 10);
+        } else if (arg_index == 3) {
+            prompt = arg;
+        }
+
+        arg_index += 1;
+    }
+
     const stdout = std.io.getStdOut().writer();
 
     var config: checkpoint.Config = undefined;
     var weights: checkpoint.Weights = undefined;
 
-    try checkpoint.readFile(allocator, "llama2_7b.bin", &config, &weights);
-    // try checkpoint.readFile(allocator, "stories110M.bin", &config, &weights);
+    try checkpoint.readFile(allocator, checkpoint_file_path orelse return error.MissingCheckpointFile, &config, &weights);
     try stdout.print("{}\n", .{config});
+
+    if (steps == 0) {
+        steps = config.seq_len;
+    }
 
     var vocab: [][]u8 = try allocator.alloc([]u8, config.vocab_size);
     var word_scores: []f32 = try allocator.alloc(f32, config.vocab_size);
 
     const max_word_length = try tokenizer.readFile(allocator, "tokenizer.bin", vocab, word_scores);
-    _ = max_word_length; // TODO
+    var prompt_tokens = try tokenizer.encodeWords(allocator, prompt, vocab, word_scores, max_word_length);
 
     var run_state: transformer.RunState = undefined;
 
@@ -37,13 +60,18 @@ pub fn main() !void {
     try stdout.print("<s>\n", .{}); // explicit print the initial BOS token for stylistic symmetry reasons
 
     var start: i64 = 0;
-    const steps = 128; // config.seq_len;
 
     for (0..steps) |pos| {
         // forward the transformer to get logits for the next token
         transformer.run(token, pos, config, &run_state, &weights);
 
-        next = argmax(run_state.logits);
+        if (prompt_tokens.len > 0) {
+            next = prompt_tokens[0];
+
+            prompt_tokens = prompt_tokens[1..];
+        } else {
+            next = argmax(run_state.logits);
+        }
 
         // following BOS token (1), sentencepiece decoder strips any leading whitespace
         const word = if (token == 1 and vocab[next][0] == ' ') vocab[next][1..] else vocab[next];
