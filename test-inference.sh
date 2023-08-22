014fce321902c18dfe1cdeeb170d4f73e6c7d2e4
Clemens Akens <clebert@me.com>
2023-08-22 11:38:14 +0200

    Enable assertions in test script

--- test-inference.sh
+++ test-inference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-actual_output=$(zig build run -Doptimize=ReleaseFast -- stories260K.bin -z tok512.bin -t 0 -n 200 --test)
+actual_output=$(zig build run -- stories260K.bin -z tok512.bin -t 0 -n 200 --test)
 
 expected_output="Once upon a time, there was a little girl named Lily. She loved to play outside in the park. One day, she saw a big, red ball. She wanted to play with it, but it was too high.
 Lily's mom said, \"Lily, let's go to the park.\" Lily was sad and didn't know what to do. She said, \"I want to play with your ball, but I can't find it.\"
