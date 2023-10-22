2a8f876d8c0564fb3d84dd2ba0935cb4d8438a2d
Clemens Akens <clebert@me.com>
2023-10-22 23:30:37 +0200

    Add multithreaded test

--- test.sh
+++ test.sh
@@ -58,4 +58,17 @@ if [ "$actual_output" != "$expected_output" ]; then
     exit 1
 fi
 
+actual_output=$(./zig-out/bin/llama2-generator $model_path --temperature 0 --sequence_length 200 --thread_count 3)
+
+# Generated with llama2.c (https://github.com/karpathy/llama2.c/tree/7ac65cb2c2b169050747be92011b7bebdd1b4544)
+expected_output="Once upon a time, there was a little girl named Lily. She loved to play outside in the park. One day, she saw a big, red ball. She wanted to play with it, but it was too high.
+Lily's mom said, \"Lily, let's go to the park.\" Lily was sad and didn't know what to do. She said, \"I want to play with your ball, but I can't find it.\"
+Lily was sad and didn't know what to do. She said, \"I'm sorry, Lily. I didn't know what to do.\"
+Lily didn't want to help her mom, so she"
+
+if [ "$actual_output" != "$expected_output" ]; then
+    echo "multithreaded test failed"
+    exit 1
+fi
+
 echo "tests ok"
